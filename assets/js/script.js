450882bcde4c29e046fedcbc2d2b2451c9f066b6
davidsalamon <salamon.david@gmail.com>
2026-03-16 19:26:30 +0100

    Cloning existing site

---
+++ assets/js/script.js
@@ -0,0 +1,196 @@
+(function ($) {
+  'use strict';
+
+  // global variables
+
+  var win = $(window);
+
+  // navigation fixed top
+  win.on('scroll', function () {
+    // Add nav-top class when scrolled down
+    if ($(window).scrollTop() > 70) {
+      $('.main-nav').addClass('nav-top');
+    } else {
+      $('.main-nav').removeClass('nav-top');
+    }
+
+    // Check if the navbar-collapse is open
+    if ($('.navbar-collapse').hasClass('show')) {
+      // Trigger collapse manually using Bootstrap's collapse method
+      $('#navbars').collapse('hide');  // Use the ID of the collapsible navbar
+    }
+  });
+
+  //Class Toggling on main content when bootstrap toggle is ON for the menubar
+  $('#navbars').on('show.bs.collapse', function () {
+    $('.main-content').addClass('menu-active');
+    $('#main-nav').addClass('menu-active');
+  });
+
+  $('#navbars').on('hide.bs.collapse', function () {
+    $('.main-content').removeClass('menu-active');
+    $('#main-nav').addClass('menu-active');
+  });
+
+  
+  // clients slider
+  $('.clients-wrap').slick({
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    infinite: true,
+    dots: false,
+    arrows: false,
+    center: true,
+    autoplay: true,
+    padding: 20,
+    autoplaySpeed: 6000,
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+          infinite: true
+        }
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      }, {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      }
+
+    ]
+  });
+
+
+  // testimonial slider
+  $('.testimonial-slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    infinite: true,
+    dots: false,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 6000,
+    responsive: [{
+      breakpoint: 900,
+      settings: {
+        slidesToShow: 2
+      }
+    }, {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1
+      }
+    }]
+  });
+
+
+  // testimonial slider 2
+  $('.testimonial-wrap').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true,
+    dots: false,
+    nextArrow: '<button class="slide-arrow prev-arrow"><i class="ti-arrow-right"></i></button>',
+    prevArrow: '<button class="slide-arrow next-arrow"><i class="ti-arrow-left"></i></button>',
+    autoplay: true,
+    autoplaySpeed: 6000
+  });
+
+
+  // magnific popup
+  $('.portfolio-gallery').each(function () {
+    $(this).find('.popup-gallery').magnificPopup({
+      type: 'image',
+      gallery: {
+        enabled: true
+      }
+    });
+  });
+
+  $(document).ready(function () {
+    $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+      disableOn: 700,
+      type: 'iframe',
+      mainClass: 'mfp-fade',
+      removalDelay: 160,
+      preloader: false,
+      fixedContentPos: false
+    });
+    $('.has-animation').each(function (index) {
+      $(this).delay($(this).data('delay')).queue(function () {
+        $(this).addClass('animate-in');
+      });
+    });
+  });
+
+
+  // search box
+  $('#searchOpen').on('click', function () {
+    $('.search-wrapper').addClass('open');
+  });
+  $('#searchClose').on('click', function () {
+    $('.search-wrapper').removeClass('open');
+  });
+
+  // Shuffle js filter and masonry
+  var containerEl = document.querySelector('.shuffle-wrapper');
+  if (containerEl) {
+    var Shuffle = window.Shuffle;
+    var myShuffle = new Shuffle(document.querySelector('.shuffle-wrapper'), {
+      itemSelector: '.shuffle-item',
+      buffer: 1
+    });
+
+    $('input[name="shuffle-filter"]').on('change', function (evt) {
+      var input = evt.currentTarget;
+      if (input.checked) {
+        myShuffle.filter(input.value);
+      }
+    });
+  }
+
+
+
+  // meta social link
+
+  $('.share-btn').on('click', function (e) {
+    e.preventDefault();
+    $('.meta-share .social-links').toggleClass('open');
+
+  });
+
+  $('div').slice(0, 6).show(); // select the first ten
+  $('#load').click(function (e) { // click event for load more
+    e.preventDefault();
+    $('div:hidden').slice(0, 3).show(); // select next 10 hidden divs and show them
+    if ($('div:hidden').length === 0) { // check if any hidden divs still exist
+      alert('No more divs'); // alert if there are none left
+    }
+  });
+
+  if ($("#search-input").length > 0) {
+    var sjs = SimpleJekyllSearch({
+      searchInput: document.getElementById('search-input'),
+      resultsContainer: document.getElementById('results-container'),
+      json: '/search.json'
+    });
+  }
+
+
+})(jQuery);
